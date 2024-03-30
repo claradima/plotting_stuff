282ed34723d107d123ed5071ab877ffd999890ce
Clara Dima <claramaria.dima@yahoo.com>
2024-03-30 20:51:02 +0000

    updated python plotting templates to mostly match Ana Sofia's root plots
    removed old .png plot files

--- python_plots_sans.py
+++ python_plots_sans.py
@@ -1,27 +1,70 @@
-# Example code with the SNO+ collaboration plot style adapted to Python
-# Requires the file Times_New_Roman_Normal.ttf
+# Python plotting template using Sans Serif (standard font in matplotlib, ok for slides but not publications)
+# This script includes some general template parameters and two example plots - one histogram + scatter with error bars + curve
+# One color map plot
+# More templates at https://github.com/claradima/plotting_stuff
+# Script tested with python 3.10.12
+# To see examples, just run python3 python_plots_sans.py
+
+# Try to stick to these quidelines if possible; for more complicated plots, try using different markers and line types to
+# distinguish between different lines/ data point types, and check out https://www.cta-observatory.org/wp-content/uploads/2020/10/CTA_ColourBlindness_BestPractices-1.pdf for colorblind friendly color palletes and other guidelines
+
+# Written by Clara Dima (c.dima@sussex.ac.uk); contact me or Ana Sofia Inacio for comments/suggestions
+
+
+
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.font_manager as fm
 import numpy as np
+from matplotlib.ticker import MultipleLocator
 
-# Set the font properties - different for paper/slides
-# Sans serif already exists in matplotlib but Times New Roman doesn't
-# So maybe we don't need font file for sans serif, just use the times new roman one?
-# Although it doesn't really seem to work ...
-
-#Setting font parameters -- this is for presentations, where sans serif is ok
+# Setting font parameters globally
+# Note - can only do this with sans serif because it already exists in matplotlib; it doesn't work for Times New Roman where we need font file
 
 plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.size'] = 12          # Default font size
-plt.rcParams['axes.titlesize'] = 20    # Title font size
-plt.rcParams['axes.labelsize'] = 16    # Axis label font size
-plt.rcParams['xtick.labelsize'] = 12   # X-axis tick label font size
-plt.rcParams['ytick.labelsize'] = 12   # Y-axis tick label font size
-plt.rcParams['legend.fontsize'] = 16   # Legend font size
+# set default font size
+plt.rcParams['font.size'] = 12
+# set title font size
+plt.rcParams['axes.titlesize'] = 20
+# set axis label font size
+plt.rcParams['axes.labelsize'] = 16
+# set tick label size on x and y axis
+plt.rcParams['xtick.labelsize'] = 14
+plt.rcParams['ytick.labelsize'] = 14
+# set legend font size
+plt.rcParams['legend.fontsize'] = 12
 
-# Define custom styles
+#set other global parameters with rcParams -- copy whole block below before plotting code to set plotting template globally
+
+# set height and width of big markings on axis x
+plt.rcParams['xtick.major.size'] = 6
+plt.rcParams['xtick.major.width'] = 1.6
+# set height and width of small markings on axis x
+plt.rcParams['xtick.minor.size'] = 3
+plt.rcParams['xtick.minor.width'] = 1.6
+# set height and width of big markings on axis y
+plt.rcParams['ytick.major.size'] = 6
+plt.rcParams['ytick.major.width'] = 1.6
+# set height and width of small markings on axis y
+plt.rcParams['ytick.minor.size'] = 3
+plt.rcParams['ytick.minor.width'] = 1.6
+# set thickness of axes
+plt.rcParams['axes.linewidth'] = 1.6
+# set plot background color
+plt.rcParams['figure.facecolor'] = 'white'
+# set plot aspect ratio -- change according to needs
+plt.rcParams['figure.figsize'] = (8.5, 6.5)
+# set padding (between ticks and axis label)
+plt.rcParams['xtick.major.pad'] = '6'
+plt.rcParams['ytick.major.pad'] = '6'
+# set padding (between plot and title)
+plt.rcParams['axes.titlepad'] = 12
+# set markings on axis to show on the inside of the plot, can change if needed
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+
+# define custom styles -- add **set_style to plot, as shown in EXAMPLE PLOT 1 below
 histogram_style = {
     'histtype': 'step',
     'color': 'blue',
@@ -30,98 +73,83 @@ histogram_style = {
 }
 
 scatter_style = {
-    'marker': 'o',
+    'marker': 's',
     'color': 'black',
-    's': 50
+    's': 25
 }
 
 errorbar_style = {
     'linestyle': 'None',
     'color': 'black',
-    'capsize': 5
+    'capsize': 1.5
 }
 
 line_plot_style = {
     'linestyle': '-',
     'color': 'red',
-    'linewidth': 1.5
+    'linewidth': 2
 }
 
-# Set global rcParams
-plt.rcParams['xtick.major.size'] = 10
-plt.rcParams['xtick.major.width'] = 1.5
-plt.rcParams['xtick.minor.size'] = 5
-plt.rcParams['xtick.minor.width'] = 1
-plt.rcParams['ytick.major.size'] = 10
-plt.rcParams['ytick.major.width'] = 1.5
-plt.rcParams['ytick.minor.size'] = 5
-plt.rcParams['ytick.minor.width'] = 1
-plt.rcParams['axes.linewidth'] = 1.5  # set the value globally
-plt.rcParams['figure.facecolor'] = 'white'
-plt.rcParams['figure.figsize'] = (18, 10)
-plt.rcParams['xtick.major.pad'] = '12'  # set padding (between ticks and axis label)
-plt.rcParams['ytick.major.pad'] = '12'
 
 # Example data for plotting
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y = [2, 4, 6, 8, 10, 9, 6.6, 7, 8.5, 3]
+y_2 = [1.3, 3.1, 5, 6, 9, 8, 6.5, 6, 7, 3.5]
 
-# First plot: hist, scatter, line
-fig, ax = plt.subplots(figsize=(18, 10))
+# EXAMPLE PLOT 1: hist, scatter, line, all on the same plot
+
+fig, ax = plt.subplots()
+# can add figsize here if globally set size is not suitable, such as:
+# fig, ax = plt.subplots(figsize=(18, 10))
 
 ax.minorticks_on()
+
+# for any of the plot types below, if you don't want to use the globally set parameters, copy parameters individually instead of writing **set_style and adjust values how you want 
+
+# plot histogram
 ax.hist(y, **histogram_style, label='Histogram')
-
+# plot line
+ax.plot(x, y_2, **line_plot_style, label='Line Plot')
+# scatter plot / data points
 ax.scatter(x, y, **scatter_style, label='Scatter')
+# plot error bars; can add param xerr to show x error as well
 ax.errorbar(x, y, yerr=0.5, **errorbar_style, label='Errorbar')
-ax.plot(x, y, **line_plot_style, label='Line Plot')
 
+# set titles 
 ax.set_xlabel('This is the x axis')
 ax.set_ylabel('This is the y axis')
 ax.set_title("Different kinds of plots!! so cool")
+# show legend; legend parameters set globally
 ax.legend()
+
+
+
+# adjusting the frequency of major ticks by changing number below;  larger number - less dense ticks
+ax.xaxis.set_major_locator(MultipleLocator(1))
+ax.yaxis.set_major_locator(MultipleLocator(1))
+
 plt.show()
 
-# Second plot: color map
-fig, ax = plt.subplots(figsize=(10, 10))
+# EXAMPLE PLOT 2: color map
+fig, ax = plt.subplots()
+# can add figsize here if globally set size is not suitable, such as:
+# fig, ax = plt.subplots(figsize=(18, 10))
 
-data = np.random.random((10, 10))
+
+# generate some random data points
+data = np.random.random((30, 30))
+
+# plot as color map
+# viridis is the default and it is colorblind friendly :)
+
 img = ax.imshow(data, cmap='viridis')
-fig.colorbar(img, ax=ax, label='Color Map')  # Add colorbar to the figure
+
+# show color map
+# pad sets the distance between plot and color bar
+# aspect sets aspect ratio of color bar;  increase the number to make it less thick
+fig.colorbar(img, ax=ax, label='Color Map',pad=0.01, aspect = 12)
 
 ax.set_xlabel('This is the x axis')
 ax.set_ylabel('This is the y axis')
 ax.set_title("Color map plot")
 plt.show()
-
-#Comments
-
-"""
-For color-blind friendly plots
-
-Color maps: default is viridis, which is already color blind friendly -- maybe just write in instructions that people shouldn't change it and just use the default?
-
-If you have different types of data points, use different markers if possible, not just different colors
-
-Sometimes we need multiple lines to be plotted in the same plot; should set colorblind friendly color cycle?
-"""
-
-"""
-Should add instructions for how to use template 
-
-Something like: if you need x type of plot, copy these lines, adjust these parameters if needed, try doing this to account for color blindness
-
-Lines ... (stuff that sets sizes of font and axis width and stuff) should not be changed
-"""
-'''
-Do we ever need to add captions for plots? Should set style for those as well?
-'''
-
-'''
-I think the title and axis label fonts are a bit too small for this plot size
-'''
-
-
-'''
-Other differences between paper/slides plots? 
-'''
--- python_plots_times.py
+++ python_plots_times.py
@@ -1,104 +1,163 @@
+# Python plotting template using Times New Roman (for publications)
+# This script includes some general template parameters and two example plots - one histogram + scatter with error bars + curve
+# One color map plot
+# To use Times New Roman font, need Times_New_Roman_Normal.ttf file 
+# File included on docDB and in github repo with plotting templates https://github.com/claradima/plotting_stuff
+# Script tested with python 3.10.12
+# To see examples, just run python3 python_plots_times.py
+
+# Try to stick to these quidelines if possible; for more complicated plots, try using different markers and line types to
+# distinguish between different lines/ data point types, and check out https://www.cta-observatory.org/wp-content/uploads/2020/10/CTA_ColourBlindness_BestPractices-1.pdf for colorblind friendly color palletes and other guidelines
+
+# Written by Clara Dima (c.dima@sussex.ac.uk); contact me or Ana Sofia Inacio for comments/suggestions
+
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.font_manager as fm
 import numpy as np
+from matplotlib.ticker import MultipleLocator
 
-# Set the font properties
+# set the font from font file
 paper_font = fm.FontProperties(fname='Times_New_Roman_Normal.ttf', size = 16)
 
-plt.rcParams['font.size'] = 12          # Default font size
-plt.rcParams['axes.titlesize'] = 20    # Title font size
-plt.rcParams['axes.labelsize'] = 16    # Axis label font size
-plt.rcParams['xtick.labelsize'] = 12   # X-axis tick label font size
-plt.rcParams['ytick.labelsize'] = 12   # Y-axis tick label font size
-plt.rcParams['legend.fontsize'] = 16   # Legend font size
+# define custom styles -- add **set_style to plot, as shown in EXAMPLE PLOT 1 below
 
-# Define custom styles
+# each of these are pretty self-explanatory, can modify them if needed
+
 histogram_style = {
-    'histtype': 'step',
+    'histtype': 'step', 
     'color': 'blue',
     'alpha': 0.7,
     'linewidth': 1.5
 }
 
 scatter_style = {
-    'marker': 'o',
+    'marker': 's',
     'color': 'black',
-    's': 50
+    's': 25
 }
 
 errorbar_style = {
     'linestyle': 'None',
     'color': 'black',
-    'capsize': 5
+    'capsize': 1.5
 }
 
 line_plot_style = {
     'linestyle': '-',
     'color': 'red',
-    'linewidth': 1.5
+    'linewidth': 2
 }
 
-# Set global rcParams
-plt.rcParams['xtick.major.size'] = 10
-plt.rcParams['xtick.major.width'] = 1.5
-plt.rcParams['xtick.minor.size'] = 5
-plt.rcParams['xtick.minor.width'] = 1
-plt.rcParams['ytick.major.size'] = 10
-plt.rcParams['ytick.major.width'] = 1.5
-plt.rcParams['ytick.minor.size'] = 5
-plt.rcParams['ytick.minor.width'] = 1
-plt.rcParams['axes.linewidth'] = 1.5  # set the value globally
+# set global rcParams -- copy whole block below before plotting code to set plotting template globally
+
+# set height and width of big markings on axis x
+plt.rcParams['xtick.major.size'] = 6
+plt.rcParams['xtick.major.width'] = 1.6
+# set height and width of small markings on axis x
+plt.rcParams['xtick.minor.size'] = 3
+plt.rcParams['xtick.minor.width'] = 1.6
+# set height and width of big markings on axis y
+plt.rcParams['ytick.major.size'] = 6
+plt.rcParams['ytick.major.width'] = 1.6
+# set height and width of small markings on axis y
+plt.rcParams['ytick.minor.size'] = 3
+plt.rcParams['ytick.minor.width'] = 1.6
+# set thickness of axes
+plt.rcParams['axes.linewidth'] = 1.6
+# set plot background color
 plt.rcParams['figure.facecolor'] = 'white'
-plt.rcParams['figure.figsize'] = (18, 10)
-plt.rcParams['xtick.major.pad'] = '12'  # set padding (between ticks and axis label)
-plt.rcParams['ytick.major.pad'] = '12'
+# set plot aspect ratio -- change according to needs
+plt.rcParams['figure.figsize'] = (8.5, 6.5)
+# set padding (between ticks and axis label)
+plt.rcParams['xtick.major.pad'] = '6'
+plt.rcParams['ytick.major.pad'] = '6'
+# set padding (between plot and title)
+plt.rcParams['axes.titlepad'] = 12
+# set markings on axis to show on the inside of the plot, can change if needed
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
 
 # Example data for plotting
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+x = [1.1, 2.5, 3.2, 4, 4.5, 6.7, 7, 8, 9, 10]
 y = [2, 4, 6, 8, 10, 9, 6.6, 7, 8.5, 3]
+y_2 = [1.3, 3.1, 5, 6, 9, 8, 6.5, 6, 7, 3.5]
 
-# First plot: hist, scatter, line
-fig, ax = plt.subplots(figsize=(18, 10))
+# EXAMPLE PLOT 1: hist, scatter, line, all on the same plot
 
-ax.minorticks_on()
-ax.hist(y, **histogram_style, label='Histogram')
-
-ax.scatter(x, y, **scatter_style, label='Scatter')
-ax.errorbar(x, y, yerr=0.5, **errorbar_style, label='Errorbar')
-ax.plot(x, y, **line_plot_style, label='Line Plot')
-
-ax.set_xlabel('This is the x axis', fontproperties=paper_font, size = 20)
-ax.set_ylabel('This is the y axis', fontproperties=paper_font, size = 20)
-ax.set_title("Different kinds of plots!! so cool", fontproperties=paper_font, size = 28)
-
-
-for label in ax.get_xticklabels():
-    label.set_fontproperties(paper_font)
-
-for label in ax.get_yticklabels():
-    label.set_fontproperties(paper_font)
-
-#make legend
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles = handles, labels = labels, prop=paper_font)
-
-
-plt.show()
-
-# Second plot: color map
 fig, ax = plt.subplots()
 
-data = np.random.random((10, 10))
-img = ax.imshow(data, cmap='viridis')
-colorbar = fig.colorbar(img, ax=ax, label='Color Map')  # Add colorbar to the figure
-# Set font properties for colorbar label
-colorbar.set_label('Color Map', fontproperties=paper_font)
+# can add figsize here if globally set size is not suitable, such as:
+# fig, ax = plt.subplots(figsize=(18, 10))
+
+ax.minorticks_on()
+
+# for any of the plot types below, if you don't want to use the globally set parameters, copy parameters individually instead of writing **set_style and adjust values how you want 
 
 
+# plot histogram
+ax.hist(y, **histogram_style, label='Histogram')
+# plor line
+ax.plot(x, y_2, **line_plot_style, label='Line Plot')
+# plot scatter plot/data points
+ax.scatter(x, y, **scatter_style, label='Scatter')
+# plot error bars, can add param xerr to show x error as well
+ax.errorbar(x, y, yerr=0.5, **errorbar_style, label='Errorbar')
+
+# for Times New Roman, need to set font sizes here instead of as a global params
 ax.set_xlabel('This is the x axis', fontproperties=paper_font, size = 20)
 ax.set_ylabel('This is the y axis', fontproperties=paper_font, size = 20)
-ax.set_title("Color map plot", fontproperties=paper_font, size = 28)
+ax.set_title("Different kinds of plots!! so cool", fontproperties=paper_font, size = 22)
+
+
+for label in ax.get_xticklabels():
+    label.set_fontproperties(paper_font)
+
+for label in ax.get_yticklabels():
+    label.set_fontproperties(paper_font)
+
+# make legend
+
+handles, labels = ax.get_legend_handles_labels()
+
+# setting legend font
+ax.legend(handles = handles, labels = labels, prop=fm.FontProperties(fname='Times_New_Roman_Normal.ttf', size = 12))
+
+# adjusting the frequency of major ticks on each axis by changing number below, larger number - less dense ticks
+ax.xaxis.set_major_locator(MultipleLocator(1))
+ax.yaxis.set_major_locator(MultipleLocator(1))
+
+
+plt.show()
+
+# EXAMPLE PLOT 2: color map
+fig, ax = plt.subplots()
+
+#can add figsize here if globally set size is not suitable, such as:
+#fig, ax = plt.subplots(figsize=(18, 10))
+
+# generate some random data to plot
+data = np.random.random((30, 30))
+
+# plot as color map
+# viridis is the default and it is colorblind friendly :)
+
+img = ax.imshow(data, cmap='viridis')
+
+# show color map
+# pad sets the distance between plot and color bar
+# aspect sets aspect ratio of color bar;  increase the number to make it less thick
+colorbar = fig.colorbar(img, ax=ax, label='Color Map', pad=0.01, aspect = 12)
+
+# set font properties for colorbar label
+colorbar.set_label('Color Map', fontproperties=paper_font)
+colorbar.ax.tick_params(labelsize=12)
+
+# for Times New Roman, need to set font sizes here instead of as a global params
+ax.set_xlabel('This is the x axis', fontproperties=paper_font, size = 20)
+ax.set_ylabel('This is the y axis', fontproperties=paper_font, size = 20)
+ax.set_title("Color map plot", fontproperties=paper_font, size = 22)
 
 for label in ax.get_xticklabels():
     label.set_fontproperties(paper_font)
@@ -107,5 +166,3 @@ for label in ax.get_yticklabels():
     label.set_fontproperties(paper_font)
 
 plt.show()
-
-
